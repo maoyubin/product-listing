2a922c22baa73798b6ff50cd7218db7fec0490fa
maoyubin <maoyb@nhn.com>
2016-08-30 16:23:35 +0800

    add js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 	
 	var displayCart = document.getElementById('display_cart');
 	var cartSection = document.getElementById('cart_section');
+	var productList = [];
+	
 
 	function visibleCart(){
 		let displayStyle = cartSection.style.display;
@@ -12,11 +14,24 @@
 			cartSection.style.display = 'block';
 			cartSection.focus();
 			cartSection.scrollIntoView();
-			//window.location.hash='#!cart_section';
-			//console.log(window.location.hash);
 		}
 	}
 	window.visibleCart = visibleCart;
+	
+	var products = document.getElementsByClassName("product-details");
+	for(var i=0;i<products.length;i++){
+		var product = {};
+		product.id = products[i].getAttribute("id");
+		var firstNode = products[i].firstElementChild;
+		var lastNode = products[i].lastElementChild;
+		var mon = lastNode.previousElementSibling.innerHTML;
+		mon = mon.substr(1, mon.length);
 
+		product.name = firstNode.innerHTML;
+		product.desc = lastNode.innerHTML;
+		product.price = mon;
+		productList.push(product);
+		console.log(product);
+	}
 	
 })(window)
