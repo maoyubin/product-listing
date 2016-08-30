7a082077af15f1c96918b277de6f6a10db371a3a
maoyubin <maoyubinabc@163.com>
2016-08-30 22:02:31 +0800

    dev js

--- js/script.js
+++ js/script.js
@@ -21,6 +21,7 @@
 	var displayCart = document.getElementById('display_cart');
 	var cartSection = document.getElementById('cart_section');
 	var addButtons = document.getElementsByClassName("add-to-cart");
+	var table = document.getElementById("cart_table");
 	var productList = [];
 	
 
@@ -44,10 +45,37 @@
 			}
 		}
 	}
+
+	function getProductNumberByID(id){
+		for(var key in cart.cartItems){
+			if(id === key){
+				return cart.cartItems[key];
+				break;
+			}
+		}
+		return 0;
+	}
+
 	window.visibleCart = visibleCart;
 	
 	function changeProductNumber(id, number){
 		
+	}
+
+	function insertTable(obj){
+		var row = table.insertRow(1);
+
+		var cell0=row.insertCell(0);
+		var cell1=row.insertCell(1);
+		var cell2=row.insertCell(2);
+		var cell3=row.insertCell(3);
+		var cell4=row.insertCell(4);
+
+		cell0.innerHTML = '<button class="close" title="Remove from shopping cart"><i class="fa fa-trash" aria-hidden="true"></i></button>';
+		cell1.innerHTML = '<img src="http://placehold.it/50x50">';
+		cell2.innerHTML = obj.desc;
+		cell3.innerHTML = '<input type="number" value="'+getProductNumberByID(obj.id)+'">';
+		cell4.innerHTML = '$'+obj.price;
 	}
 	
 	var products = document.getElementsByClassName("product-details");
@@ -83,7 +111,17 @@
 		   }
 		   
 		   document.getElementById('share_box_number').innerHTML = cart.numberOfItems();
+
+		   insertTable(getProductByID(productID));
 	    });
 	}
+
+	table.onclick = function(event) {
+		if (event.target.nodeName == 'I') {
+		    var i = event.target;
+		    var rowIndex = i.parentNode.parentNode.parentNode.rowIndex;
+		    table.deleteRow(rowIndex);
+		}
+	};
 	
 })(window)
